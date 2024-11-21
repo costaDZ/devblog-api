6976809221b9114a7a4451a11d5bb6ea96da3863
Nadjem <A.Najemaddin@naseej.com>
2024-11-21 17:49:11 +0100

    Add module type to package json file

--- src/index.ts
+++ src/index.ts
@@ -5,9 +5,11 @@ import connectToDB from "./config/db";
 import blogRoutes from "./routes/blogRoutes";
 import authRoutes from "./routes/authRoutes";
 import {verifyToken} from "@middlewares/authMiddleware";
+require("dotenv").config({path: `.env.${process.env.NODE_ENV || "development"}`});
 
 const app = express();
 const PORT = process.env.PORT || 5000;
+AbortController;
 
 app.use(express.json());
 
